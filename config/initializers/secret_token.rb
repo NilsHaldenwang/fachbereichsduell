9a19f8b5d118ff6a6df376d4dfa08ec3d5a5cfb9
Nils Haldenwang <n.haldenwang@googlemail.com>
2012-11-05 15:06:19 +0100

    Rails app angelegt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fachbereichsduell::Application.config.secret_token = '3fc717b7af384181c7bab0096d67f497bf3172411b9054397fff5872ba212b3a986379f9c4b74114a13954de5ad3659c7c3afacdee7692d10430d1d40ac5e913'
