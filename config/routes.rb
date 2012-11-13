b81bc45e5ae64a554e27855446069eed5460de03
Nicolas Neubauer <nicolas.neubauer@me.com>
2012-11-13 16:49:09 +0100

    Sound und doppelte Route

--- config/routes.rb
+++ config/routes.rb
@@ -77,7 +77,7 @@ Fachbereichsduell::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
-  root :to => 'questions#show'
+  # root :to => 'questions#show'
 
   # See how all your routes lay out with "rake routes"
 
